35284f4de6a17d401099d03b161a3acedbb92f80
CHF <huafeng.chen7@outlook.com>
2023-04-04 21:34:45 +0800

    Repair some bugs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python AS mycontainer
 COPY chatbot_v0.py /
 COPY requirements.txt /
+COPY config.json /
 RUN ["pip", "install", "pip", "update"]
 RUN ["pip", "install", "-r", "requirements.txt"]
+ENV ACCESS_TOKEN=6097704169:AAGXJq3uZXedkZSlsphbqeFq48Ts-1eZfes
 CMD python chatbot_v0.py
--- chatbot_v0.py
+++ chatbot_v0.py
@@ -27,7 +27,8 @@ def main():
     # Load your token and create an Updater for your Bot
     # config = configparser.ConfigParser()
     # config.read('config.ini')
-    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
+    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True) 
     dispatcher = updater.dispatcher
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
